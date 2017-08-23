006e56ce5f4ddc2cb79985dce6aa99267c751f14
Moshe Ben-Ezra <moshe@ben-ezra.org>
2017-08-23 01:07:15 -0400

    rm extra ;

--- src/copyright.h
+++ src/copyright.h
@@ -13,6 +13,6 @@
 #ifndef COPYRIGHT
 #define COPYRIGHT
 static const char __attribute__((used)) *copyright =
-"@(#) Clip    (c) 2007 The Hebrew University of Jerusalem\n";
+"@(#) Clip    (c) 2007 The Hebrew University of Jerusalem\n"
 "@(#) ClipOCV (c) 2017 Moshe Ben-Ezra \n";
 #endif

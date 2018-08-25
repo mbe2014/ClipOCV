fb83331e3123e81f386e6c20a0118659e7008ec1
Moshe Ben-Ezra <moshe@snookerx.com>
2018-08-25 16:31:58 -0400

    add simple test

---
+++ test.sh
@@ -0,0 +1,20 @@
+#!/bin/csh -v
+rm -rf testdir
+mkdir testdir
+cd testdir
+ln -s ../build/release/example/* .
+ln -s ../example/lena.ppm .
+
+./display < lena.ppm 
+./display1 lena.ppm 
+./invert < lena.ppm > inv.ppm ; 	display inv.ppm
+./fft < lena.ppm > fft.ppm ; 		display fft.ppm
+./mirror < lena.ppm > mirror.ppm ; 	display mirror.ppm
+./mirror1 < lena.ppm > mirror1.ppm ; 	display mirror1.ppm
+./pow0_5 < lena.ppm > pow.ppm ; 	display pow.ppm
+./sobel1 < lena.ppm > sobel1.ppm ; 	display sobel1.ppm
+./sobel2 < lena.ppm > sobel2.ppm ; 	display sobel2.ppm
+./togray < lena.ppm > gray.ppm ; 	display gray.ppm
+./togray1 < lena.ppm > gray1.ppm ; 	display gray1.ppm
+./warp < lena.ppm > warp.ppm ; 		display warp.ppm
+
